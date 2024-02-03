5eebd2d69af851a7847dfc07670ed3b0d7cd1e85
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-02-03 12:30:57 -0500

    other values loading in

--- src/app/profComponents/class-data-prof/class-data-prof.component.ts
+++ src/app/profComponents/class-data-prof/class-data-prof.component.ts
@@ -12,11 +12,12 @@ import {
 })
 export class ClassDataProfComponent implements OnInit {
 
-  courseOvl: number | undefined = 0;
   strokeColor: string = '#6d1f22';
   profName: string | undefined = undefined;
 
   professorCourses: CourseTableData[] | undefined = undefined;
+  firstHalfCourses: CourseTableData[] = [];
+  secondHalfCourses: CourseTableData[] = [];
 
   constructor(private prof: ProfessorService) {}
 
@@ -34,6 +35,14 @@ export class ClassDataProfComponent implements OnInit {
 
     this.prof.getcrsTableData().subscribe((data: CourseTableData[] | null) => {
       this.professorCourses = data || undefined;
+
+      if (this.professorCourses) {
+        const halfLength = Math.ceil(this.professorCourses.length / 2);
+        
+        // Slice the array into two halves
+        this.firstHalfCourses = this.professorCourses.slice(0, halfLength);
+        this.secondHalfCourses = this.professorCourses.slice(halfLength);
+      }
 
     });
   }
