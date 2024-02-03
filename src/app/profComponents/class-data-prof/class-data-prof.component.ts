58b6cc7ce9b2578551f14364822b59f8c883bda2
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-02-03 00:31:24 -0500

    template

--- src/app/profComponents/class-data-prof/class-data-prof.component.ts
+++ src/app/profComponents/class-data-prof/class-data-prof.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ProfessorService,
+  ProfPageData,
+} from 'src/app/PageDataService/professor.service';
 
 @Component({
   selector: 'app-class-data-prof',
   templateUrl: './class-data-prof.component.html',
   styleUrls: ['./class-data-prof.component.css']
 })
-export class ClassDataProfComponent {
+export class ClassDataProfComponent implements OnInit {
 
-  courseOvl: number | undefined = 50;
+  courseOvl: number | undefined = NaN;
   strokeColor: string = '#6d1f22';
+
+  constructor(private prof: ProfessorService) {}
+
+
+
 
 }
