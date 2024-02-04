5c5a0b74c7c393331859854381703c19f3590261
andrew <boessena@bc.edu>
2024-02-03 19:40:45 -0500

    add export

--- src/app/PageDataService/profile.service.ts
+++ src/app/PageDataService/profile.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-interface Comment {
+export interface Comment {
   user_id?: string;
   message: string;
   createdAt: Date;
