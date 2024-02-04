5944d8ab90c738a3e973c38b9a6db7622b83ca72
Andrew Boessen <boessena@bc.edu>
2024-02-04 15:48:24 -0500

    refresh prof table data

--- src/app/class-page-entry/class-page-entry.component.ts
+++ src/app/class-page-entry/class-page-entry.component.ts
@@ -1,6 +1,9 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ClassService,CoursePageData } from 'src/app/PageDataService/class.service';
+import {
+  ClassService,
+  CoursePageData,
+} from 'src/app/PageDataService/class.service';
 import { CollectDataService } from '../collect-data/collect-data.service';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -20,11 +23,13 @@ export class ClassrPageEntryComponent {
   ngOnInit() {
     // Set prof data to null
     this.classService.setCoursePageData(null);
+    this.classService.setprofTableData(null);
 
     // First get the product id from the current route.
     this.route.paramMap.subscribe((routeParams) => {
       // Set prof data to null
       this.classService.setCoursePageData(null);
+      this.classService.setprofTableData(null);
 
       const id = String(routeParams.get('classId'));
 
@@ -32,13 +37,10 @@ export class ClassrPageEntryComponent {
       this.data.getCoursePageData(id);
     });
 
-    this.classService.getCoursePageData().subscribe((data: CoursePageData | null) => {
-      this.courseCode = data?.crs_code || undefined;
-  
-    });
-
-     
-
-
+    this.classService
+      .getCoursePageData()
+      .subscribe((data: CoursePageData | null) => {
+        this.courseCode = data?.crs_code || undefined;
+      });
   }
 }
