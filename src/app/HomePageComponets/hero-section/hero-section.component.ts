1645f4c8ac066641b779e7f1b79d18b192a10d0c
andrew <boessena@bc.edu>
2024-10-12 11:43:41 -0400

    add html

--- src/app/HomePageComponets/hero-section/hero-section.component.ts
+++ src/app/HomePageComponets/hero-section/hero-section.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { AppSettings } from 'src/app/appSettings';
 
 @Component({
   selector: 'app-hero-section',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class HeroSectionComponent {
   inputValue: string = '';
+
+  countReviews: number | undefined = undefined;
+  countCourses: number | undefined = undefined;
+  countProfs: number | undefined = undefined;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    const reviewUrl = AppSettings.API_ENDPOINT + 'fetch/database/reviews/count';
+    const courseUrl = AppSettings.API_ENDPOINT + 'fetch/database/course/count';
+    const profUrl = AppSettings.API_ENDPOINT + 'fetch/database/prof/count';
+
+    this.apiService.getCount(reviewUrl).subscribe((data: any | null) => {
+      this.countReviews = data?.count || undefined;
+    });
+    this.apiService.getCount(courseUrl).subscribe((data: any | null) => {
+      this.countCourses = data?.count || undefined;
+    });
+    this.apiService.getCount(profUrl).subscribe((data: any | null) => {
+      this.countProfs = data?.count / 2 || undefined;
+    });
+  }
 }
