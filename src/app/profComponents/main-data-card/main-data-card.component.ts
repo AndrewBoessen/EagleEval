3e3b0ba604e314c051eab4eca16436fc57b86d2b
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2026-06-10 11:17:29 -0400

    Main Data card mock data added

--- src/app/profComponents/main-data-card/main-data-card.component.ts
+++ src/app/profComponents/main-data-card/main-data-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {
   ProfessorService,
   ProfPageData,
@@ -10,22 +10,28 @@ import {
   styleUrls: ['./main-data-card.component.css'],
 })
 export class MainDataCardComponent implements OnInit {
-  professorOvl: number | undefined = NaN;
-  strokeColor: string = '#6d1f22';
-  instructorPrepared: number | undefined = undefined;
-  clearMaterial: number | undefined = undefined;
-  outsideClass: number | undefined = undefined;
-  enthusiastic: number | undefined = undefined;
 
-  constructor(private prof: ProfessorService) {}
+  strokeColor: string = '#6d1f22';
+  @Input() professorOvl: number | undefined = NaN;
+  @Input() instructorPrepared: number | undefined = undefined;
+  @Input() clearMaterial: number | undefined = undefined;
+  @Input() outsideClass: number | undefined = undefined;
+  @Input() enthusiastic: number | undefined = undefined;
+
+  constructor(private prof: ProfessorService) { }
 
   ngOnInit() {
-    this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
-      this.professorOvl = data?.avgOverall || undefined;
-      this.instructorPrepared = data?.avgPrepared || undefined;
-      this.clearMaterial = data?.avgExplains || undefined;
-      this.outsideClass = data?.avgAvailable || undefined;
-      this.enthusiastic = data?.avgEnthusiastic || undefined;
-    });
+
+    const coreFields = [this.professorOvl, this.instructorPrepared, this.clearMaterial, this.outsideClass, this.enthusiastic];
+
+    if (coreFields.some(field => !field)) {
+      this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
+        this.professorOvl = data?.avgOverall || undefined;
+        this.instructorPrepared = data?.avgPrepared || undefined;
+        this.clearMaterial = data?.avgExplains || undefined;
+        this.outsideClass = data?.avgAvailable || undefined;
+        this.enthusiastic = data?.avgEnthusiastic || undefined;
+      });
+    }
   }
 }
