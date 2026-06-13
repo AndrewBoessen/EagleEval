46e7fe10390060bbcce53f9f4c880966d5c52d5e
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2026-06-12 23:47:08 -0400

    Basic init of stats page

---
+++ src/app/site-statistics/site-stats-page/site-stats-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-stats-page',
+  templateUrl: './site-stats-page.component.html',
+  styleUrls: ['./site-stats-page.component.css']
+})
+export class SiteStatsPageComponent {
+
+}
